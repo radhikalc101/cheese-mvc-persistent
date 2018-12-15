65acc2d31bb3595deea449b1a8a62ea43bfe01d1
Rsdhika <radheram831@gmail.com>
2018-12-15 14:16:29 -0600

    added index,add html and created category controller first part done

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,51 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    // requestpath:/category
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+        model.addAttribute("categories",categoryDao.findAll());
+        model.addAttribute("title", "Categories");
+        return "category/index";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addForm(Model model){
+        model.addAttribute("title","Add category");
+        model.addAttribute(new Category());
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add",method = RequestMethod.POST)
+    public String addForm( @ModelAttribute Category category, Errors errors, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute("title","Add category");
+            return "category/add";
+        }
+        categoryDao.save(category);
+        return "redirect:";
+    }
+
+
+
+
+}
